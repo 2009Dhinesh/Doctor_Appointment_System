395a17589807b4f48a9ccfcab202f3ca7e2de005
Dhinesh waran <dwaranp@gmail.com>
2025-05-02 12:36:49 +0530

    Update server.js with latest changes

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+app.get('/',()=>console.log("Hello World"))
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
